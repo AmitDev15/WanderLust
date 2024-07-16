eeef384710eccfdb8f72777ca1e40118be9977b2
Amit Kumar <amitk.singh1506@gmail.com>
2024-07-16 20:25:55 +0530

    Fix issue in show comments

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const PORT = 8080;
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbURL = process.env.MONGO_ATLAS_URL || "";
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -36,9 +35,9 @@ async function main() {
 }
 main().catch((err) => console.log(err));
 
-// app.get("/", (req, res) => {
-//   res.send("Main Page");
-// });
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
 //! Mongo Store Session
 const store = MongoStore.create({
